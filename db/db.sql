74fbebc292eb6845722545b9409ef41cf8d1b7df
HarrySun123 <23592@redeemer.com.au>
2019-08-03 15:25:23 +1000

    init

---
+++ db/db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Users(
+user_id varchar(10) NOT NULL,
+user_name varchar(255) NOT NULL,
+user_year varchar(10) NOT NULL,
+user_email varchar(255) NOT NULL,
+user_phone varchar(30) NOT NULL,
+user_role varchar(255) NOT NULL,
+user_tutor_group varchar(255) NOT NULL,
+);
+
+
+CREATE TABLE Sessions(
+session_id varchar(20) NOT NULL,
+mentor_id varchar(10) NOT NULL,
+mentee_id varchar(10) NULL,
+mentor_year varchar(10) NOT NULL,
+mentee_year varchar(10) NULL,
+subject varchar(255) NOT NULL,
+time varchar(255) NOT NULL,
+location varchar(255) NULL,
+status varchar(10) NOT NULL,
+feedback varchar(1000) NOT NULL,
+);
